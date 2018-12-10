91a17df2e78fde2203eb23365380626352a7b770
Matheus Lessa <matheus@fastmail.com>
2018-12-10 14:05:06 -0200

    more types

--- src/lib.rs
+++ src/lib.rs
@@ -75,3 +75,45 @@ impl Header {
 		Ok(header)
 	}
 }
+
+pub struct Frame {
+	pub byte_count: u32,
+	pub _magic_number: u16,
+	pub number_of_chunks_old: u16,
+	pub frame_duration_milliseconds: u16,
+	pub _ignored0: [u8; 2],
+	pub number_of_chunks_new: u32,
+}
+
+impl Frame {
+	pub fn read<R: Read>(read: &mut R) -> Result<Self> {
+		let frame = Frame {
+			byte_count: read.read_u32::<LittleEndian>()?,
+			_magic_number: read.read_u16::<LittleEndian>()?,
+			number_of_chunks_old: read.read_u16::<LittleEndian>()?,
+			frame_duration_milliseconds: read.read_u16::<LittleEndian>()?,
+			_ignored0: [read.read_u8()?, read.read_u8()?],
+			number_of_chunks_new: read.read_u32::<LittleEndian>()?,
+		};
+
+		Ok(frame)
+	}
+}
+
+pub struct Chunk {
+	pub chunk_size: u32,
+	pub chunk_type: u16,
+	pub chunk_data: Vec<u8>,
+}
+
+impl Chunk {
+	pub fn read<R: Read>(read: &mut R) -> Result<Self> {
+		let chunk = Chunk {
+			chunk_size: read.read_u32::<LittleEndian>()?,
+			chunk_type: read.read_u16::<LittleEndian>()?,
+			chunk_data: Vec::new(), //read.read_u16::<LittleEndian>()?,
+		};
+
+		Ok(chunk)
+	}
+}
