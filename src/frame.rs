79e655a16b1d5cd717a7d2385a8bb968d21e029f
Matheus Lessa <matheus@fastmail.com>
2019-01-08 21:50:32 -0200

    final function that reads an Aseprite file

--- src/frame.rs
+++ src/frame.rs
@@ -1,16 +1,18 @@
+use std::io::{self, Read, Seek, SeekFrom};
+
 use byteorder::{LittleEndian, ReadBytesExt};
 
-use std::io::{self, Read, Seek, SeekFrom};
+use crate::{Chunk, Header};
 
 pub struct Frame {
 	pub byte_count: u32,
-	pub number_of_chunks_old: u16,
 	pub frame_duration_milliseconds: u16,
-	pub number_of_chunks_new: u32,
+	pub number_of_chunks: u32,
+	pub chunks: Vec<Chunk>,
 }
 
 impl Frame {
-	pub fn from_read<R>(read: &mut R) -> io::Result<Self>
+	pub fn from_read<R>(read: &mut R, header: &Header) -> io::Result<Self>
 	where
 		R: Read + Seek,
 	{
@@ -21,11 +23,22 @@ impl Frame {
 		read.seek(SeekFrom::Current(2))?;
 		let number_of_chunks_new = read.read_u32::<LittleEndian>()?;
 
+		let number_of_chunks = if number_of_chunks_new == 0 {
+			number_of_chunks_old as u32
+		} else {
+			number_of_chunks_new
+		};
+
+		let mut chunks = Vec::with_capacity(number_of_chunks as usize);
+		for _ in 0..number_of_chunks {
+			chunks.push(Chunk::from_read(read, header)?);
+		}
+
 		Ok(Self {
 			byte_count,
-			number_of_chunks_old,
 			frame_duration_milliseconds,
-			number_of_chunks_new,
+			number_of_chunks,
+			chunks,
 		})
 	}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::io::{Read, Result};
+use std::io::{self, Read, Seek};
 
 mod header;
 pub use self::header::*;
@@ -17,21 +17,41 @@ https://github.com/aseprite/aseprite/blob/master/docs/ase-file-specs.md
 
 ASE files use Intel (little-endian) byte order.
 
-	BYTE (u8): An 8-bit unsigned integer value
-	WORD (u16): A 16-bit unsigned integer value
-	SHORT (i16): A 16-bit signed integer value
-	DWORD (u32): A 32-bit unsigned integer value
-	LONG (i32): A 32-bit signed integer value
-	FIXED (f32): A 32-bit fixed point (16.16) value
-	BYTE[n] ([u8; n]): "n" bytes.
-	STRING:
-		WORD: string length (number of bytes)
-		BYTE[length]: characters (in UTF-8) The '\0' character is not included.
-	PIXEL: One pixel, depending on the image pixel format:
-		RGBA: BYTE[4], each pixel have 4 bytes in this order Red, Green, Blue, Alpha.
-		Grayscale: BYTE[2], each pixel have 2 bytes in the order Value, Alpha.
-		Indexed: BYTE, Each pixel uses 1 byte (the index).
+BYTE (u8): An 8-bit unsigned integer value
+WORD (u16): A 16-bit unsigned integer value
+SHORT (i16): A 16-bit signed integer value
+DWORD (u32): A 32-bit unsigned integer value
+LONG (i32): A 32-bit signed integer value
+FIXED (f32): A 32-bit fixed point (16.16) value
+BYTE[n] ([u8; n]): "n" bytes.
+STRING:
+	WORD: string length (number of bytes)
+	BYTE[length]: characters (in UTF-8) The '\0' character is not included.
+PIXEL: One pixel, depending on the image pixel format:
+	RGBA: BYTE[4], each pixel have 4 bytes in this order Red, Green, Blue, Alpha.
+	Grayscale: BYTE[2], each pixel have 2 bytes in the order Value, Alpha.
+	Indexed: BYTE, Each pixel uses 1 byte (the index).
 */
+
+pub struct Aseprite {
+	pub header: Header,
+	pub frames: Vec<Frame>,
+}
+
+impl Aseprite {
+	pub fn from_read<R>(read: &mut R) -> io::Result<Aseprite>
+	where
+		R: Read + Seek,
+	{
+		let header = Header::from_read(read)?;
+		let mut frames = Vec::with_capacity(header.frames as usize);
+		for _ in 0..header.frames {
+			frames.push(Frame::from_read(read, &header)?);
+		}
+
+		Ok(Self { header, frames })
+	}
+}
 
 #[cfg(test)]
 mod tests {
@@ -39,8 +59,4 @@ mod tests {
 	fn it_works() {
 		assert_eq!(2 + 2, 4);
 	}
-}
-
-pub fn read<R: Read>(_read: &mut R) -> Result<()> {
-	Ok(())
 }
